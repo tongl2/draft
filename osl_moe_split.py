b549f22de31e207467ccb36facb8109109bfc3e7
tongl <liu.tong1@outlook.com>
2025-07-31 11:53:37 +0800

    osl_moe_split.py

--- osl_moe_split.py
+++ osl_moe_split.py
@@ -10,18 +10,18 @@ from tqdm.contrib.concurrent import process_map
 MOE_PARAM_PATTERN = r'.feed_forward.routed_experts.ffn.'
 NUM_EXPERTS = 256
 
-def get_file_list(param_map):
+def get_moe_file_list(param_map):
     file_set = set()
     for param_name, file_name in param_map.items():
         if MOE_PARAM_PATTERN in param_name:
             file_set.add(file_name)
     return list(file_set)
 
-def get_param_map(safetensors_path):
-    return json.load(open(f'{safetensors_path}/param_name_map.json', 'r'))
+def get_param_map():
+    return json.load(open(f'{args.input_path}/param_name_map.json', 'r'))
 
-def save_param_map(param_map, output_path):
-    with open(f'{output_path}/param_name_map.json', 'w') as f:
+def save_param_map(param_map):
+    with open(f'{args.output_path}/param_name_map.json', 'w') as f:
         json.dump(param_map, f, indent=4)
 
 def split_moe_params(origin_name, moe_slice):
@@ -51,7 +51,7 @@ def proc_safetensor_file(filename):
     save_file(file_params, f'{args.output_path}/{filename}')
 
 def proc_param_map(param_map):
-    for k in tqdm(param_map):
+    for k in tqdm(list(param_map)):
         if MOE_PARAM_PATTERN in k:
             origin_name_parts = k.split('.')
             for i in range(NUM_EXPERTS):
@@ -60,14 +60,14 @@ def proc_param_map(param_map):
             del param_map[k]
 
 def main():
-    param_map = get_param_map(args.input_path)
-    file_map = get_file_list(param_map)
+    param_map = get_param_map()
+    file_map = get_moe_file_list(param_map)
     print(f'Found {len(file_map)} files with MOE parameters.')
     print(f'Processing {len(file_map)} files...')
     process_map(proc_safetensor_file, file_map, max_workers=32)
     print('Processing complete. Now processing parameter map...')
-    param_map = proc_param_map(param_map)
-    save_param_map(param_map, args.output_path)
+    proc_param_map(param_map)
+    save_param_map(param_map)
     print('Done!')
 
 if __name__ == "__main__":
